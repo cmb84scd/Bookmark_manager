5738adaa2d82f49c78f4e6764595ac980d536314
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-06 18:14:28 +0100

    CRUD

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require_relative './lib/bookmarks.rb'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions, :method_override
+
   get '/bookmarks' do
     @bookmarks = Bookmarks.all
     erb(:index)
@@ -13,6 +15,12 @@ class BookmarkManager < Sinatra::Base
 
   post '/bookmarks' do
     Bookmarks.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+
+  delete '/bookmarks/:id' do
+    p params
+    Bookmarks.delete(id: params[:id])
     redirect '/bookmarks'
   end
 
