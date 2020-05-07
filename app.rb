ea931b05310870af23e50d384ac5f0dea6fd155d
Jed Stanton <jedstanton@hotmail.com>
2020-05-07 14:52:21 +0100

    Update method added to model

--- app.rb
+++ app.rb
@@ -29,8 +29,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   patch '/bookmarks/:id' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("UPDATE bookmarks SET url = '#{params[:url]}', title = '#{params[:title]}' WHERE id = '#{params[:id]}'")
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url]) 
     redirect('/bookmarks')
   end
 
